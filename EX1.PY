ebb2ac93831c6c8aff0283b9f6f62571c18b0f90
VictorSouza17 <paulo.mendes35@aluno.ce.gov.br>
2025-12-04 12:53:40 -0300

    adicionei uma condicao

--- EX1.PY
+++ EX1.PY
@@ -3,5 +3,8 @@ idade_mais_novo = 0
 for i in range(10):
   nome = input('Digite seu nome: ')
   idade = int(input('Digite sua idade: '))
+  if ( False ):
+    nome_mais_novo = nome
+    idade_mais_novo = idade
 print(nome_mais_novo)  
 print(idade_mais_novo) 
